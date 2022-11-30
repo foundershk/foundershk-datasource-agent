9fdaf8b1a10567f482113f1cf83d29f35ce40128
foundershk <99274058+foundershk@users.noreply.github.com>
2022-11-30 19:13:32 +0000

    Version 3.2.2

--- cmd/pdc/main.go
+++ cmd/pdc/main.go
@@ -43,3 +43,47 @@ type mainFlags struct {
 	Domain    string
 
 	// The fields below were added to make local development easier.
+	//
+	// DevMode is true when the agent is being run locally while someone is working on it.
+	DevMode bool
+}
+
+func (mf *mainFlags) RegisterFlags(fs *flag.FlagSet) {
+	fs.BoolVar(&mf.PrintHelp, "h", false, "Print help")
+	fs.StringVar(&mf.LogLevel, "log.level", logLevelinfo, `"debug", "info", "warn" or "error"`)
+	fs.StringVar(&mf.Cluster, "cluster", "", "the PDC cluster to connect to use")
+	fs.StringVar(&mf.Domain, "domain", "grafana.net", "the domain of the PDC cluster")
+	fs.BoolVar(&mf.DevMode, "dev-mode", false, "[DEVELOPMENT ONLY] run the agent in development mode")
+}
+
+func logLevelToSSHLogLevel(level string) (int, error) {
+	switch level {
+	case "error", "warn", "info":
+		return 0, nil
+	case "debug":
+		return 3, nil
+	default:
+		return -1, fmt.Errorf("invalid log level: %s", level)
+	}
+}
+
+// Tries to get the openssh version. Returns "UNKNOWN" on error.
+func tryGetOpenSSHVersion() string {
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	buffer := bytes.NewBuffer([]byte{})
+
+	cmd := exec.CommandContext(timeoutCtx, "ssh", "-V")
+	// ssh -V outputs to stderr.
+	cmd.Stderr = buffer
+
+	if err := cmd.Run(); err != nil {
+		return "UNKNOWN"
+	}
+
+	// ssh -V adds \n to the end of the output.
+	return strings.Replace(buffer.String(), "\n", "", 1)
+}
+
+func main() {
