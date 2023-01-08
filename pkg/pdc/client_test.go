9f6a8027f0fdc66749b053257f19f1bde9d06d6d
foundershk <99274058+foundershk@users.noreply.github.com>
2023-01-08 20:35:06 +0000

    bump version to 0.0.3

--- pkg/pdc/client_test.go
+++ pkg/pdc/client_test.go
@@ -52,4 +52,6 @@ azl0ZGNvOFFqN0pIcFR0WnFBRm12c1E9PQo=
 -----END CERTIFICATE-----
 `
 
-func TestSigningResponse_UnmarshalJSON(t *
+func TestSigningResponse_UnmarshalJSON(t *testing.T) {
+	testcases := []struct {
+		name        strin
