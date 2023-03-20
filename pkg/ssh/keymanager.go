3e516feeb63480bbba5b5681d08e72d2131ac7f1
foundershk <99274058+foundershk@users.noreply.github.com>
2023-03-20 17:55:08 +0000

    adding zoom, maptiles_geoext_test.html

--- pkg/ssh/keymanager.go
+++ pkg/ssh/keymanager.go
@@ -129,4 +129,4 @@ func (km KeyManager) newKeysRequired() bool {
 
 	block, _ := pem.Decode(kb)
 	if block == nil {
-		level.Info(km.logger).Log("msg", "new keys req
+		level.Info(km.logger).Log("msg", "new keys required: could not parse private key PEM file")
