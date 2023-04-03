7e42ec657102ac842eb85438996c5fd4fadbd5ee
foundershk <99274058+foundershk@users.noreply.github.com>
2023-04-03 14:17:04 +0000

    cpufreq: cpu-boost: Use hotplug thread infrastructure

--- pkg/ssh/keymanager.go
+++ pkg/ssh/keymanager.go
@@ -185,4 +185,4 @@ func (km KeyManager) newCertRequired() bool {
 	}
 	_, _, _, _, _, err = ssh.ParseKnownHosts(kh)
 	if err != nil {
-		level.Info
+		level.Info(km.logger).Log("msg", fmt.Sprintf("fetchi
