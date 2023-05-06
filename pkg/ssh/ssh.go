7f89fe65a89aee905c0d7deb3b69f654a72d4760
foundershk <99274058+foundershk@users.noreply.github.com>
2023-05-06 19:41:06 +0000

    Version bump to 1.2.5

--- pkg/ssh/ssh.go
+++ pkg/ssh/ssh.go
@@ -99,3 +99,55 @@ func NewClient(cfg *Config, logger log.Logger, km *KeyManager) *Client {
 		cfg:    cfg,
 		SSHCmd: "ssh",
 		logger: logger,
+		km:     km,
+	}
+
+	client.BasicService = services.NewIdleService(client.starting, client.stopping)
+	return client
+}
+
+func (s *Client) starting(ctx context.Context) error {
+	level.Info(s.logger).Log("msg", "starting ssh client")
+
+	// check keys and cert validity before start, create new cert if required
+	// This will exit if it fails, rather than endlessly retrying to sign keys.
+	if s.km != nil {
+		err := s.km.CreateKeys(ctx)
+		if err != nil {
+			level.Error(s.logger).Log("msg", "could not check or generate certificate", "error", err)
+			return err
+		}
+	}
+
+	// Attempt to parse SSH flags before triggering the goroutine, so we can exit
+	// if the parsing fails
+	flags, err := s.SSHFlagsFromConfig()
+	if err != nil {
+		level.Error(s.logger).Log("msg", fmt.Sprintf("could not parse flags: %s", err))
+		return err
+	}
+	level.Debug(s.logger).Log("msg", fmt.Sprintf("parsed flags: %s", flags))
+
+	retryOpts := retry.Opts{MaxBackoff: 16 * time.Second, InitialBackoff: 1 * time.Second}
+	go retry.Forever(retryOpts, func() error {
+		cmd := exec.CommandContext(ctx, s.SSHCmd, flags...)
+		loggerWriter := newLoggerWriterAdapter(s.logger)
+		cmd.Stdout = loggerWriter
+		cmd.Stderr = loggerWriter
+		_ = cmd.Run()
+		if ctx.Err() != nil {
+			return nil // context was canceled
+		}
+
+		if cmd.ProcessState != nil && cmd.ProcessState.ExitCode() == ConnectionLimitReachedCode {
+			level.Info(s.logger).Log("msg", "limit of connections for stack and network reached. exiting")
+			os.Exit(1)
+		}
+
+		level.Error(s.logger).Log("msg", "ssh client exited. restarting")
+
+		// Check keys and cert validity before restart, create new cert if required.
+		// This covers the case where a certificate has become invalid since the last start.
+		// Do not return here: we want to keep trying to connect in case the PDC API
+		// is temporarily unavailable.
+		if s.km != nil {
