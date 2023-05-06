f6c82f1457c503da3db7ef9411d929ef117449cf
foundershk <99274058+foundershk@users.noreply.github.com>
2023-05-06 14:43:34 +0000

    readme file update

--- pkg/ssh/ssh.go
+++ pkg/ssh/ssh.go
@@ -41,3 +41,61 @@ type Config struct {
 	// ForceKeyFileOverwrite forces a new ssh key pair to be generated.
 	ForceKeyFileOverwrite bool
 	URL                   *url.URL
+}
+
+// DefaultConfig returns a Config with some sensible defaults set
+func DefaultConfig() *Config {
+	root, err := os.UserHomeDir()
+	if err != nil {
+		// Use relative path (should not happen)
+		root = ""
+	}
+	return &Config{
+		Port:     22,
+		LogLevel: 2,
+		PDC:      pdc.Config{},
+		KeyFile:  path.Join(root, ".ssh/grafana_pdc"),
+	}
+}
+
+func (cfg *Config) RegisterFlags(f *flag.FlagSet) {
+	var deprecatedInt int
+
+	def := DefaultConfig()
+
+	cfg.SSHFlags = []string{}
+	f.StringVar(&cfg.KeyFile, "ssh-key-file", def.KeyFile, "The path to the SSH key file.")
+	f.IntVar(&deprecatedInt, "log-level", def.LogLevel, "[DEPRECATED] Use the log.level flag. The level of log verbosity. The maximum is 3.")
+	// use default log level if invalid
+	if cfg.LogLevel > 3 {
+		cfg.LogLevel = def.LogLevel
+	}
+	f.Func("ssh-flag", "Additional flags to be passed to ssh. Can be set more than once.", cfg.addSSHFlag)
+	f.BoolVar(&cfg.ForceKeyFileOverwrite, "force-key-file-overwrite", false, "Force a new ssh key pair to be generated")
+}
+
+func (cfg Config) KeyFileDir() string {
+	dir, _ := path.Split(cfg.KeyFile)
+	return dir
+}
+
+func (cfg *Config) addSSHFlag(s string) error {
+	cfg.SSHFlags = append(cfg.SSHFlags, s)
+	return nil
+}
+
+// Client is a client for ssh. It configures and runs ssh commands
+type Client struct {
+	*services.BasicService
+	cfg    *Config
+	SSHCmd string // SSH command to run, defaults to "ssh". Require for testing.
+	logger log.Logger
+	km     *KeyManager
+}
+
+// NewClient returns a new SSH client in an idle state
+func NewClient(cfg *Config, logger log.Logger, km *KeyManager) *Client {
+	client := &Client{
+		cfg:    cfg,
+		SSHCmd: "ssh",
+		logger: logger,
